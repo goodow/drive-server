d33c1074cb738245825793914bac8db2344849ce
larry <i@goodow.com>
2014-04-17 14:03:02 +0800

    Signify bootstrap verticle to be considered started until the other modules and verticles have been started
    Change-Id: If4e46fa03861060a82110d0b96118b49d0bd33da

--- src/main/java/com/goodow/drive/server/bootstrap/DriveVerticle.java
+++ src/main/java/com/goodow/drive/server/bootstrap/DriveVerticle.java
@@ -9,25 +9,29 @@ import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Future;
 
 public class DriveVerticle extends BusModBase {
+  private int count;
+
   @Override
   public void start(final Future<Void> startedResult) {
     super.start();
-
-    container.deployVerticle(DriveWebServer.class.getName(), config,
-        new AsyncResultHandler<String>() {
-          @Override
-          public void handle(AsyncResult<String> event) {
-            if (event.succeeded()) {
-              startedResult.setResult(null);
-            } else {
-              startedResult.setFailure(event.cause());
-            }
+    AsyncResultHandler<String> doneHandler = new AsyncResultHandler<String>() {
+      @Override
+      public void handle(AsyncResult<String> ar) {
+        if (ar.succeeded()) {
+          count++;
+          if (count == 4) {
+            startedResult.setResult(null);
           }
-        });
+        } else {
+          startedResult.setFailure(ar.cause());
+        }
+      }
+    };
+    container
+        .deployModule("com.goodow.realtime~realtime-store~0.5.5-SNAPSHOT", config, doneHandler);
 
-    container.deployModule("com.goodow.realtime~realtime-store~0.5.5-SNAPSHOT", config);
-
-    container.deployVerticle(PlayerAnalyticsVerticle.class.getName(), config);
-    container.deployVerticle(GeoAuthenticate.class.getName(), config);
+    container.deployVerticle(DriveWebServer.class.getName(), config, doneHandler);
+    container.deployVerticle(PlayerAnalyticsVerticle.class.getName(), config, doneHandler);
+    container.deployVerticle(GeoAuthenticate.class.getName(), config, doneHandler);
   }
 }
