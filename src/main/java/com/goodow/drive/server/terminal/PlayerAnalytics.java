550df1bc47dd4309b7c0e6cefcd01dec47b8f246
Benjamin <shangmin@goodow.com>
2014-05-05 13:09:39 +0800

    修改后台逻辑，更改AttachmentActivity与DeviceActivity的数据模型
    Change-Id: Ie9a47e2317fd37285123532716d3c286c7857343

--- src/main/java/com/goodow/drive/server/terminal/PlayerAnalytics.java
+++ src/main/java/com/goodow/drive/server/terminal/PlayerAnalytics.java
@@ -25,7 +25,7 @@ import org.vertx.java.busmods.BusModBase;
  * @Des:对终端提交的播放统计的数据进行转换和存储
  */
 @GuiceVertxBinding(modules = {DriveModule.class})
-public class PlayerAnalytics extends BusModBase {
+public class PlayerAnalytics extends BusModBase implements MyConstant{
   @Inject
   private Bus bus;
   private int countDownLatch = 0;
@@ -34,35 +34,51 @@ public class PlayerAnalytics extends BusModBase {
   public void start() {
     GuiceVerticleHelper.inject(this, vertx, container);
     super.start();
-    bus.registerHandler(MyConstant.ADDR_PLAYER, new MessageHandler<JsonObject>() {
+    bus.registerHandler(ADDR_PLAYER, new MessageHandler<JsonObject>() {
       @Override
       public void handle(Message<JsonObject> rootMessage) {
         JsonObject body = rootMessage.body();
-        String sid = body.getString("sid");
-        JsonArray analyticsArray = Json.createArray();
-        JsonArray analytics = body.getArray("analytics");
-        for (int i = 0; i < analytics.length(); i++) {
-          JsonObject object = analytics.getObject(i);
-          String attachmentId = object.getString("attachment");
-          JsonArray timestamps = object.getArray("timestamp");
-          for (int j = 0; j < timestamps.length(); j++) {
-            JsonObject timestampObject = timestamps.getObject(j);
-            long openTime = (long) timestampObject.getNumber("openTime");
-            long lastTime = ((long) timestampObject.getNumber("lastTime") / 1000);
-            analyticsArray.push(Json.createObject().set("user", sid).set("attachmentId",
-                attachmentId).set("open ", DateUtil.parseTimestamp(openTime)).set("duration",
-                lastTime));
-            countDownLatch++;
-          }
-        }
-        savePlayAnalytics(rootMessage, analyticsArray);
+        final String sid = body.getString("sid");
+        final JsonArray analytics = body.getArray("analytics");
+          JsonObject sidQuery =
+                  Json.createObject().set("action", "get").set("_index",ES_INDEX).set("_type",ES_TYPE_DEVICE).set("_id", sid);
+          bus.send(SEARCH_CHANNEL,sidQuery, new MessageHandler<JsonObject>() {
+              @Override
+              public void handle(Message<JsonObject> rootMessage) {
+                  JsonObject body = rootMessage.body();
+                  final String owner = body.getObject("_source").getString("owner");
+                  for (int i = 0; i < analytics.length(); i++) {
+                      JsonObject object = analytics.getObject(i);
+                      final String attachmentId = object.getString("attachment");
+                      final JsonArray timestamps = object.getArray("timestamp");
+                      JsonObject attachementQuery = Json.createObject().set("action","get").set("_index",ES_INDEX).set("_type",ES_TYPE_ATTACHMENT).set("_id", attachmentId);
+                      bus.send("realtime.search",attachementQuery, new MessageHandler<JsonObject>() {
+                          @Override
+                          public void handle(Message<JsonObject> rootMessage) {
+                              final JsonArray analyticsArray = Json.createArray();
+                              String title = rootMessage.body().getObject("_source").getString("title");
+                              for (int j = 0; j < timestamps.length(); j++) {
+                                  JsonObject timestampObject = timestamps.getObject(j);
+                                  long openTime = (long) timestampObject.getNumber("openTime");
+                                  long lastTime = (long) timestampObject.getNumber("lastTime");
+                                  analyticsArray.push(Json.createObject().set("userId", sid).set("attachmentId",
+                                          attachmentId).set("title",title).set("open ", DateUtil.parseTimestamp(openTime)).set("duration",
+                                          lastTime).set("user",owner));
+                                  countDownLatch++;
+                              }
+                              savePlayAnalytics(rootMessage, analyticsArray);
+                          }
+                      });
+                  }
+              }
+          });
       }
     });
   }
 
   /**
    * 保存转换后的数据
-   * 
+   *
    * @author:DingPengwei
    * @date:Apr 21, 2014 3:21:59 PM
    * @param rootMessage
@@ -71,9 +87,9 @@ public class PlayerAnalytics extends BusModBase {
   private void savePlayAnalytics(final Message<JsonObject> rootMessage, JsonArray analyticsArray) {// rx.java
     for (int i = 0; i < analyticsArray.length(); i++) {
       JsonObject msg =
-          Json.createObject().set("action", "index").set("_index", MyConstant.ES_INDEX).set(
-              "_type", MyConstant.ES_TYPE_PLAYER).set("source", analyticsArray.getObject(i));
-      bus.send("realtime.search", msg, new MessageHandler<JsonObject>() {
+          Json.createObject().set("action", "index").set("_index", ES_INDEX).set(
+              "_type", ES_TYPE_PLAYER).set("source", analyticsArray.getObject(i));
+      bus.send(SEARCH_CHANNEL, msg, new MessageHandler<JsonObject>() {
         @Override
         public void handle(Message<JsonObject> messageDb) {
           countDownLatch--;
